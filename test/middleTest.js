29c981a8210c626e4a3c159078fe5f666a4e1af0
Abdul Rahman <lhlrahman@gmail.com>
2023-07-10 22:51:07 -0400

    Refactored

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,27 +1,21 @@
-const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
+const middle = require('../middle');
 
+describe("middle", () => {
+  it("should return [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
 
-// Test case 1: Array with odd length
-const arr1 = [1, 2, 3, 4, 5];
-const expected1 = [3];
-const actual1 = middle(arr1);
-assertArraysEqual(actual1, expected1);
+  it("should return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
 
-// Test case 2: Array with even length
-const arr2 = [1, 2, 3, 4];
-const expected2 = [2, 3];
-const actual2 = middle(arr2);
-assertArraysEqual(actual2, expected2);
+  it("should return [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-// Test case 3: Array with length less than 3
-const arr3 = [1];
-const expected3 = [];
-const actual3 = middle(arr3);
-assertArraysEqual(actual3, expected3);
+  it("should return [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
 
-// Test case 4: Empty array
-const arr4 = [];
-const expected4 = [];
-const actual4 = middle(arr4);
-assertArraysEqual(actual4, expected4);
+});
