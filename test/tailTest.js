f97d23c1ccc6f2e657fe7f27a7afcfadd4828d35
Abdul Rahman <lhlrahman@gmail.com>
2023-07-10 22:51:14 -0400

    Refactored

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,11 @@
+const assert = require('chai').assert;
+const tail = require('../tail');
 
-const assertEqual = require("../assertEqual");
-const tail = require("../tail");
+describe("tail", () => {
+  it("should not modify the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
 
-// TEST CODE
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+});
